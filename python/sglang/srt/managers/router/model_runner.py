9b3b6ecfdce89270b201bb9c72adfe27fe247908
Qubitium <417764+Qubitium@users.noreply.github.com>
2024-03-13 12:32:18 +0000

    Fix env compat: lxd container may not be able to start sglang due empty address

--- python/sglang/srt/managers/router/model_runner.py
+++ python/sglang/srt/managers/router/model_runner.py
@@ -263,6 +263,9 @@ class ModelRunner:
         global global_server_args_dict
         global_server_args_dict = server_args_dict
 
+        os.environ["MASTER_ADDRESS"] = "127.0.0.1"
+        os.environ["MASTER_PORT"] = str(self.nccl_port)
+
         # Init torch distributed
         torch.cuda.set_device(self.tp_rank)
         print(f"Starting torch.distributed.init_process_group at tcp://0.0.0.0:{self.nccl_port}")
@@ -270,7 +273,7 @@ class ModelRunner:
             backend="gloo",
             world_size=self.tp_size,
             rank=self.tp_rank,
-            init_method=f"tcp://127.0.0.1:{self.nccl_port}",
+            # init_method=f"tcp://127.0.0.1:{self.nccl_port}",
         )
 
         # A small all_reduce for warmup.
