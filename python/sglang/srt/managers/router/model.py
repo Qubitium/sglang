682cc0bdadfe0ed12e6f1802a831a8839338270b
Qubitium <417764+Qubitium@users.noreply.github.com>
2024-03-19 05:45:44 +0000

    Remove unused var

--- python/sglang/srt/managers/router/model.py
+++ python/sglang/srt/managers/router/model.py
@@ -81,7 +81,6 @@ class ModelServer():
                 tokenizer_mode=server_args.tokenizer_mode,
                 trust_remote_code=server_args.trust_remote_code,
             )
-        self.eos_token_id = self.tokenizer.eos_token_id
         self.max_total_num_token = self.model_runner.max_total_num_token
         self.max_num_running_seq = self.max_total_num_token // 2
         self.max_prefill_num_token = max(
