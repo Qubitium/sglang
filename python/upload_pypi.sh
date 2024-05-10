1069c0a20b688d6090421ed8942c369a4f609474
ZX <zx@lbx.dev>
2024-05-10 11:16:46 +0000

    update version

--- python/upload_pypi.sh
+++ python/upload_pypi.sh
@@ -4,6 +4,6 @@ python update_version.py
 git add pyproject.toml
 python3 -m build
 #python3 -m twine upload dist/*
-rm /monster/data/pkg/sglang*.whl
+#rm /monster/data/pkg/sglang*.whl
 rm -rf README.md LICENSE
 mv dist/*.whl /monster/data/pkg/
