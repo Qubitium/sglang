418056e30dbec84befef0bfb2fad4134234d6ac6
PZS-ModelCloud <pzs@modelcloud.ai>
2024-08-19 06:00:59 +0000

    remove multimodal_config

--- python/sglang/srt/model_executor/model_runner.py
+++ python/sglang/srt/model_executor/model_runner.py
@@ -219,7 +219,6 @@ class ModelRunner:
             device_config=device_config,
             load_config=load_config,
             lora_config=None,
-            multimodal_config=None,
             parallel_config=None,
             scheduler_config=None,
             cache_config=None,
