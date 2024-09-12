d09ec9ee819b2b5d2ce00e7db430ec7d238b9f65
ZX <zx@lbx.dev>
2024-09-12 11:41:47 +0000

    compat vllm change

--- python/sglang/srt/models/chatglm.py
+++ python/sglang/srt/models/chatglm.py
@@ -38,7 +38,7 @@ from vllm.model_executor.layers.vocab_parallel_embedding import (
 )
 from vllm.model_executor.model_loader.weight_utils import default_weight_loader
 from vllm.model_executor.sampling_metadata import SamplingMetadata
-from vllm.sequence import SamplerOutput
+from vllm.model_executor.layers.sampler import SamplerOutput
 from vllm.transformers_utils.configs import ChatGLMConfig
 
 from sglang.srt.layers.activation import SiluAndMul
